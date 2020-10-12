cc126c4441f821b4ea6fe991b7110181ff8b603e
datadesigns <paul@datadesigns.co.uk>
2020-10-12 15:12:29 +0000

    update

---
+++ scripts/colab_nb1.py
@@ -0,0 +1,25 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: notebooks///ipynb,scripts///py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.6.0
+#   kernelspec:
+#     display_name: Python 3
+#     name: python3
+# ---
+
+# %% [markdown] id="view-in-github" colab_type="text"
+# <a href="https://colab.research.google.com/github/ptcane/grading/blob/master/colab_nb1.ipynb" target="_parent"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/></a>
+
+# %% id="pCv4Sg1fPP5D"
+x = 77
+y = 55
+
+# %% id="b_StdEFmPTzj"
+z = x + y + 2
+
+# %% id="Jvk5_DUOPWGA"
